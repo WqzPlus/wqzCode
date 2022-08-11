33c5eb50b3ea55bb49b992d8b57afe46ca8696f5
wqz <wangqiongzhou@farben.com.cn>
2022-08-11 14:59:17 +0800

    后台管理系统

---
+++ src/views/Donate.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div class="crumbs">
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item>
+                    <i class="el-icon-lx-redpacket_fill"></i> 支持作者
+                </el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+        <div class="container">
+            <div class="plugins-tips">如果该框架对你有帮助，那就请作者喝杯饮料吧！加微信号wqz17600268337探讨问题。</div>
+            <div  >
+                <img src="screenshots\c044107170edda4e4b0a230a85283fa.jpg" style="height: 300px;width: 260px"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "donate"
+};
+</script>
+
+<style>
+</style>
